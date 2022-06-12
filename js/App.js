d3400ad64716deac04fdf1b9452fe964a77ca175
Juttu Govind Rao <jgovindrao13@gmail.com>
2022-06-13 04:07:24 +0530

    Added initial styles and container and heading in html

--- js/App.js
+++ js/App.js
@@ -1,1 +1,18 @@
-console.log("App file");
+import React from "react";
+
+export default (props) => {
+  return (
+    <React.Fragment>
+      <div class="container">
+        <h1 class="heading">Amity Delivery Services</h1>
+        <div class="content">
+          <form>
+            <input />
+            <input />
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
